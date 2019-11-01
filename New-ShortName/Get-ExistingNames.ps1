6bf3f3143504a570c1176998263140fc4a378f58
warrentc3 <26634421+warrentc3@users.noreply.github.com>
2019-11-01 10:09:56 -0400

    Delete Get-ExistingNames.ps1

--- New-ShortName/Get-ExistingNames.ps1
+++
@@ -1,7 +0,0 @@
-
-$AzUsers = (Get-AzureADUser -All $true)
-$AzUsers = $AzUsers | Where-Object {$_.UserType -eq "Member"}
-$usermails = $AzUsers.ProxyAddresses | Where-Object {$_ -like "smtp*"} | ForEach-Object {$_.ToLower() -replace "smtp:",""}
-$names = $usermails | ForEach-Object {($_ -Split("@"))[0]} 
-$names += $AzUsers.MailNickName.ToLower()
-$names = ($names | Sort-Object | Group-Object).Where({ $_.Name -ne " "}).Name
